544c416c467aaf7e9dbcdab47bcfd9bf44254657
pyDream <401196518@qq.com>
2018-01-02 10:39:29 +0800

    add alembic init table

---
+++ warder/db/migration/alembic/versions/414e0160cac0_create_user_table.py
@@ -0,0 +1,24 @@
+"""create user table
+
+Revision ID: 414e0160cac0
+Revises: 
+Create Date: 2018-01-02 10:34:52.576471
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '414e0160cac0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ warder/db/migration/alembic/versions/60cc99926b3a_create_telephone_table.py
@@ -0,0 +1,24 @@
+"""create telephone table
+
+Revision ID: 60cc99926b3a
+Revises: 414e0160cac0
+Create Date: 2018-01-02 10:35:26.507981
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '60cc99926b3a'
+down_revision = '414e0160cac0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
