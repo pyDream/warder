8ffb86a16e9c736451273d3940471a1e1d0343d8
yangyq <401196518@qq.com>
2018-01-04 18:34:46 +0800

    add create user

--- warder/db/data_models.py
+++ warder/db/data_models.py
@@ -91,14 +91,13 @@ class User(BaseDataModel):
         return "<User(user_id='%s', name='%s', gender='%s',age='%s',email='%s')>" % (
             self.user_id, self.name, self.gender, self.age, self.email)
 
-    def __init__(self, id, user_id, name, gender, age, email, telephone):
+    def __init__(self, id, user_id, name, gender, age, email):
         self.id = id
         self.user_id = user_id
         self.name = name
         self.gender = gender
         self.age = age
         self.email = email
-        self.telephone = telephone
 
 
 class Telephone(BaseDataModel):
@@ -106,6 +105,7 @@ class Telephone(BaseDataModel):
         return "<Telephone(user_id='%s', telnumber='%s')>" % (
             self.user_id, self.telnumber)
 
-    def __init__(self, user_id, telnumber):
+    def __init__(self, id, user_id, telnumber):
+        self.id = id
         self.user_id = user_id
         self.telnumber = telnumber
